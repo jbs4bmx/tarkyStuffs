52cda5e7a9bdc4d60f6e9a0361b6e6f18f4fc5d8
Jace <livingfern1@live.com>
2022-02-01 08:50:34 -0500

    Update Module
    Compatibility update for AKI 2.2.0

--- modules/HoltzmanShield/HoltzmanShield/Properties/AssemblyInfo.cs
+++ modules/HoltzmanShield/HoltzmanShield/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("jbs4bmx.HoltzmanShield")]
+[assembly: AssemblyTitle("HoltzmanShield")]
 [assembly: AssemblyDescription("Single Player Tarkov mod. Modifies ArmorSlot value to allow items placed into the ArmBand slot to count as armor.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("jbs4bmx.HoltzmanShield")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCompany("jbs4bmx")]
+[assembly: AssemblyProduct("HoltzmanShield")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.2.0")]
-[assembly: AssemblyFileVersion("2.1.2.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
---
+++ modules/HoltzmanShield/HoltzmanShield/Shield.cs
@@ -0,0 +1,45 @@
+﻿using Aki.Common.Utils;
+using EFT.InventoryLogic;
+using System.Reflection;
+
+/* ::NOTES::
+ * ...
+ * Author: jbs4bmx
+ * Tester(s): "@kiki", "@Phantom in Time"
+ * Date: 31 Jan, 2022
+ * Update: 1 Feb, 2022
+ * ...
+*/
+
+namespace HoltzmanShield
+{
+    public class Shield
+    {
+        public static void Main(string[] args)
+        {
+            Log.Info("HoltzmanShield: Applying armor patch...");
+            AddArmBandArmorSlot();
+
+            Log.Info("HoltzmanShield: Patch successfully applied");
+        }
+
+        public static void AddArmBandArmorSlot()
+        {
+            var bindingFlags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public;
+
+            var field = typeof(InventoryClass).GetField("ArmorSlots", bindingFlags);
+
+            field.SetValue(null, new EquipmentSlot[]
+            {
+                EquipmentSlot.TacticalVest,
+                EquipmentSlot.ArmorVest,
+                EquipmentSlot.Headwear,
+                EquipmentSlot.FaceCover,
+                EquipmentSlot.Eyewear,
+                EquipmentSlot.ArmBand
+            });
+
+            Log.Info($"HoltzmanShield: Applied static patch {nameof(AddArmBandArmorSlot)}");
+        }
+    }
+}
