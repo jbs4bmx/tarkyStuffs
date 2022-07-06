83717d57506bde85fdefb8c7c88a921407de5fc3
Jace <livingfern1@live.com>
2022-07-05 21:10:01 -0400

    Mass Update
    Add more shields.

--- mods/HoltzmanShield/user/mods/HoltzmanShield/src/shield.ts
+++ mods/HoltzmanShield/user/mods/HoltzmanShield/src/shield.ts
@@ -51,24 +51,28 @@ class Holtzman implements IMod
                 locales[localeID].templates[locale] = mydb.locales.en.templates[locale];
             }
         }
-        items["55d7217a4bdc2d86028b456d"]._props.Slots[14]._props.filters[0].Filter.push("HShield");
+        items["55d7217a4bdc2d86028b456d"]._props.Slots[14]._props.filters[0].Filter.push(
+            "HShieldEvade",
+            "HShieldTG",
+            "HShieldUSEC",
+            "HShieldYellow",
+            "HShieldUntar",
+            "HShieldRed",
+            "HShieldWhite",
+            "HShieldRivals",
+            "HShieldAlpha",
+            "HShieldRFArmy",
+            "HShieldTrainHard",
+            "HShieldGreen",
+            "HShieldBlue",
+            "HShieldKiba",
+            "HShieldDead",
+            "HShieldLabs",
+            "HShieldBear"
+        );
 
         let armor = [];
         let segments = [];
-        var itemId = "HShield";
-        var itemPrefabPath = "assets/content/items/equipment/armband/item_equipment_armband_evasion.bundle";
-        var itemLongName = "Holtzman Shield";
-        var itemShortName = "HS";
-        var itemProto = "545cdb794bdc2d3a198b456a";
-        var itemClone = "572b7fa524597762b747ce82";
-        var itemParent = "57bef4c42459772e8d35a53b";
-        var itemCategory = "5b5f701386f774093f2ecf0f";
-        var itemFleaPrice = Resources.marketPrice;
-        var itemDescription = "You've read Dune right? Or at least seen the movies? Right?...";
-        var itemTrader = "5ac3b934156ae10c4430e83c";
-        var itemTraderPrice = Resources.traderPrice;
-        var itemTraderCurrency = "5449016a4bdc2d6f028b456f";
-        var itemTraderLV = Resources.minTraderLevel;
 
         if (typeof MainArmor.Head === "boolean") { if (MainArmor.Head === true) { armor.push("Head") } } else { armor.push("Head") }
         if (typeof MainArmor.Thorax === "boolean") { if (MainArmor.Thorax === true) { armor.push("Chest") } } else { armor.push("Chest") }
@@ -84,20 +88,126 @@ class Holtzman implements IMod
         if (typeof HeadAreas.Jaws === "boolean") { if (HeadAreas.Jaws === true) { segments.push("Jaws") } } else { segments.push("Jaws") }
         if (typeof Resources.RepairCost === "number") { if ((Resources.RepairCost < 1) || (Resources.RepairCost > 9999999)) { Resources.RepairCost = 1000 } } else { Resources.RepairCost = 1000 }
         if (typeof Resources.Durability === "number") { if ((Resources.Durability < 1) || (Resources.Durability > 9999999)) { Resources.Durability = 1500 } } else { Resources.Durability = 1500 }
-        if (typeof Resources.minTraderLevel === "number") { if ((Resources.minTraderLevel < 1) || (Resources.minTraderLevel > 4)) { Resources.minTraderLevel = 1 } } else { Resources.minTraderLevel = 1 }
-        if (typeof Resources.marketPrice === "number") { if ((Resources.marketPrice < 1) || (Resources.marketPrice > 9999999)) { Resources.marketPrice = 69420 } } else { Resources.marketPrice = 69420 }
         if (typeof Resources.traderPrice === "number") { if ((Resources.traderPrice < 1) || (Resources.traderPrice > 9999999)) { Resources.traderPrice = 69420 } } else { Resources.traderPrice = 69420 }
 
-        items[itemId]._proto = itemProto;
-        items[itemId]._props.CreditsPrice = itemTraderPrice;
-        items[itemId]._props.RepairCost = Resources.RepairCost;
-        items[itemId]._props.Durability = Resources.Durability;
-        items[itemId]._props.MaxDurability = Resources.Durability;
-        items[itemId]._props.armorClass = "10";
-        items[itemId]._props.armorZone = armor;
-        items[itemId]._props.headSegments = segments;
-        items[itemId]._props.MaterialType = "BodyArmor";
-        items[itemId]._props.ArmorMaterial = "UHMWPE";
+        items["HShieldEvade"]._props.CreditsPrice = Resources.traderPrice;
+        items["HShieldEvade"]._props.RepairCost = Resources.RepairCost;
+        items["HShieldEvade"]._props.Durability = Resources.Durability;
+        items["HShieldEvade"]._props.MaxDurability = Resources.Durability;
+        items["HShieldEvade"]._props.armorZone = armor;
+        items["HShieldEvade"]._props.headSegments = segments;
+
+        items["HShieldTG"]._props.CreditsPrice = Resources.traderPrice;
+        items["HShieldTG"]._props.RepairCost = Resources.RepairCost;
+        items["HShieldTG"]._props.Durability = Resources.Durability;
+        items["HShieldTG"]._props.MaxDurability = Resources.Durability;
+        items["HShieldTG"]._props.armorZone = armor;
+        items["HShieldTG"]._props.headSegments = segments;
+
+        items["HShieldUSEC"]._props.CreditsPrice = Resources.traderPrice;
+        items["HShieldUSEC"]._props.RepairCost = Resources.RepairCost;
+        items["HShieldUSEC"]._props.Durability = Resources.Durability;
+        items["HShieldUSEC"]._props.MaxDurability = Resources.Durability;
+        items["HShieldUSEC"]._props.armorZone = armor;
+        items["HShieldUSEC"]._props.headSegments = segments;
+
+        items["HShieldYellow"]._props.CreditsPrice = Resources.traderPrice;
+        items["HShieldYellow"]._props.RepairCost = Resources.RepairCost;
+        items["HShieldYellow"]._props.Durability = Resources.Durability;
+        items["HShieldYellow"]._props.MaxDurability = Resources.Durability;
+        items["HShieldYellow"]._props.armorZone = armor;
+        items["HShieldYellow"]._props.headSegments = segments;
+
+        items["HShieldUntar"]._props.CreditsPrice = Resources.traderPrice;
+        items["HShieldUntar"]._props.RepairCost = Resources.RepairCost;
+        items["HShieldUntar"]._props.Durability = Resources.Durability;
+        items["HShieldUntar"]._props.MaxDurability = Resources.Durability;
+        items["HShieldUntar"]._props.armorZone = armor;
+        items["HShieldUntar"]._props.headSegments = segments;
+
+        items["HShieldRed"]._props.CreditsPrice = Resources.traderPrice;
+        items["HShieldRed"]._props.RepairCost = Resources.RepairCost;
+        items["HShieldRed"]._props.Durability = Resources.Durability;
+        items["HShieldRed"]._props.MaxDurability = Resources.Durability;
+        items["HShieldRed"]._props.armorZone = armor;
+        items["HShieldRed"]._props.headSegments = segments;
+
+        items["HShieldWhite"]._props.CreditsPrice = Resources.traderPrice;
+        items["HShieldWhite"]._props.RepairCost = Resources.RepairCost;
+        items["HShieldWhite"]._props.Durability = Resources.Durability;
+        items["HShieldWhite"]._props.MaxDurability = Resources.Durability;
+        items["HShieldWhite"]._props.armorZone = armor;
+        items["HShieldWhite"]._props.headSegments = segments;
+
+        items["HShieldRivals"]._props.CreditsPrice = Resources.traderPrice;
+        items["HShieldRivals"]._props.RepairCost = Resources.RepairCost;
+        items["HShieldRivals"]._props.Durability = Resources.Durability;
+        items["HShieldRivals"]._props.MaxDurability = Resources.Durability;
+        items["HShieldRivals"]._props.armorZone = armor;
+        items["HShieldRivals"]._props.headSegments = segments;
+
+        items["HShieldAlpha"]._props.CreditsPrice = Resources.traderPrice;
+        items["HShieldAlpha"]._props.RepairCost = Resources.RepairCost;
+        items["HShieldAlpha"]._props.Durability = Resources.Durability;
+        items["HShieldAlpha"]._props.MaxDurability = Resources.Durability;
+        items["HShieldAlpha"]._props.armorZone = armor;
+        items["HShieldAlpha"]._props.headSegments = segments;
+
+        items["HShieldRFArmy"]._props.CreditsPrice = Resources.traderPrice;
+        items["HShieldRFArmy"]._props.RepairCost = Resources.RepairCost;
+        items["HShieldRFArmy"]._props.Durability = Resources.Durability;
+        items["HShieldRFArmy"]._props.MaxDurability = Resources.Durability;
+        items["HShieldRFArmy"]._props.armorZone = armor;
+        items["HShieldRFArmy"]._props.headSegments = segments;
+
+        items["HShieldTrainHard"]._props.CreditsPrice = Resources.traderPrice;
+        items["HShieldTrainHard"]._props.RepairCost = Resources.RepairCost;
+        items["HShieldTrainHard"]._props.Durability = Resources.Durability;
+        items["HShieldTrainHard"]._props.MaxDurability = Resources.Durability;
+        items["HShieldTrainHard"]._props.armorZone = armor;
+        items["HShieldTrainHard"]._props.headSegments = segments;
+
+        items["HShieldGreen"]._props.CreditsPrice = Resources.traderPrice;
+        items["HShieldGreen"]._props.RepairCost = Resources.RepairCost;
+        items["HShieldGreen"]._props.Durability = Resources.Durability;
+        items["HShieldGreen"]._props.MaxDurability = Resources.Durability;
+        items["HShieldGreen"]._props.armorZone = armor;
+        items["HShieldGreen"]._props.headSegments = segments;
+
+        items["HShieldBlue"]._props.CreditsPrice = Resources.traderPrice;
+        items["HShieldBlue"]._props.RepairCost = Resources.RepairCost;
+        items["HShieldBlue"]._props.Durability = Resources.Durability;
+        items["HShieldBlue"]._props.MaxDurability = Resources.Durability;
+        items["HShieldBlue"]._props.armorZone = armor;
+        items["HShieldBlue"]._props.headSegments = segments;
+
+        items["HShieldKiba"]._props.CreditsPrice = Resources.traderPrice;
+        items["HShieldKiba"]._props.RepairCost = Resources.RepairCost;
+        items["HShieldKiba"]._props.Durability = Resources.Durability;
+        items["HShieldKiba"]._props.MaxDurability = Resources.Durability;
+        items["HShieldKiba"]._props.armorZone = armor;
+        items["HShieldKiba"]._props.headSegments = segments;
+
+        items["HShieldDead"]._props.CreditsPrice = Resources.traderPrice;
+        items["HShieldDead"]._props.RepairCost = Resources.RepairCost;
+        items["HShieldDead"]._props.Durability = Resources.Durability;
+        items["HShieldDead"]._props.MaxDurability = Resources.Durability;
+        items["HShieldDead"]._props.armorZone = armor;
+        items["HShieldDead"]._props.headSegments = segments;
+
+        items["HShieldLabs"]._props.CreditsPrice = Resources.traderPrice;
+        items["HShieldLabs"]._props.RepairCost = Resources.RepairCost;
+        items["HShieldLabs"]._props.Durability = Resources.Durability;
+        items["HShieldLabs"]._props.MaxDurability = Resources.Durability;
+        items["HShieldLabs"]._props.armorZone = armor;
+        items["HShieldLabs"]._props.headSegments = segments;
+
+        items["HShieldBear"]._props.CreditsPrice = Resources.traderPrice;
+        items["HShieldBear"]._props.RepairCost = Resources.RepairCost;
+        items["HShieldBear"]._props.Durability = Resources.Durability;
+        items["HShieldBear"]._props.MaxDurability = Resources.Durability;
+        items["HShieldBear"]._props.armorZone = armor;
+        items["HShieldBear"]._props.headSegments = segments;
 
         logger.info("Holtzman Shield Mod: Cached Successfully");
     }
