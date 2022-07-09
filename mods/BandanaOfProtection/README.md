728e2f6c681a6d683cb08f400123a6cd00ba37f1
Jace <livingfern1@live.com>
2022-07-09 11:12:50 -0400

    Options Update
    Options Update

--- mods/BandanaOfProtection/README.md
+++ mods/BandanaOfProtection/README.md
@@ -4,8 +4,13 @@ This mod adds a new version of the bandana (half-mask) and adds armor protection
 
 >Author  : jbs4bmx
 
->Version : 3.0.0 (Supports SPT-AKI 3.0.0 and EFT Version 0.12.12.15.18346)
+>Version : 3.0.1 (Supports SPT-AKI 3.0.0 and EFT Version 0.12.12.15.18346)
 
+
+### UPDATE 3.0.1
+Now adds every face cover option in the game. Choose which face cover you prefer to use by editing the options in the configuration file.
+Only enable 1 option though... if you enable more than one, the last one in the list that is enabled, will be used.
+Default is the original mod's bandana (HalfMask) option.
 
 
 ### Configuration Guide
@@ -17,6 +22,8 @@ You can specify the following configurations in the "\config\config.json" file.
    - For RepairCost, marketPrice, and traderPrice, any value below 1 or greater than 9999999 will default to 100000.
    - For Durability, values below 1 or greater than 9999999 will default to 100.
    - For minTraderLevel, values below 1 or greater than 4 will default to 2.
+3. "FaceCover" allows you to choose your favorite look.
+   - Last option set to true will be used so remember to only set 1 option to TRUE.
 
 ``` json
 {
@@ -40,6 +47,45 @@ You can specify the following configurations in the "\config\config.json" file.
         "RepairCost": 1,        // [Integer] value must be a whole number - Sets the cost to repair the item. (1-9999999)
         "Durability": 1,        // [Integer] value must be a whole number - Sets the durability amount of the item. (1-9999999)
         "traderPrice": 1        // [Integer] value must be a whole number - Sets the Ragman price of the item. (1-9999999)
-    }
+    },
+    "FaceCover": {
+        "HalfMask": true,                   // [Boolean] value must be set to true or false - true = enabled.
+        "GP5GasMask": false,                // [Boolean]
+        "GP7GasMask": false,                // [Boolean]
+        "Respirator": false,                // [Boolean]
+        "DevBalaclava": false,              // [Boolean]
+        "JasonMask": false,                 // [Boolean]
+        "MichealMask": false,               // [Boolean]
+        "PestilyMask": false,               // [Boolean]
+        "SmokeBalaclava": false,            // [Boolean]
+        "TagillaGorilla": false,            // [Boolean]
+        "TagillaUBEY": false,               // [Boolean]
+        "GhostBalaclava": false,            // [Boolean]
+        "MomexBalaclava": false,            // [Boolean]
+        "ColdFearBalaclava": false,         // [Boolean]
+        "Rivals2021Balaclava": false,       // [Boolean]
+        "Balaclava": false,                 // [Boolean]
+        "RoninBallistic": false,            // [Boolean]
+        "TwitchRivals2020Mask": false,      // [Boolean]
+        "TwitchRivals2020HalfMask": false,  // [Boolean]
+        "GreenShemagh": false,              // [Boolean]
+        "TanShemagh": false,                // [Boolean]
+        "ShroudMask": false,                // [Boolean]
+        "ShatteredMask": false,             // [Boolean]
+        "DeadlySkull": false,               // [Boolean]
+        "NeopreneMask": false,              // [Boolean]
+        "GhoulMask": false,                 // [Boolean]
+        "SlenderMask": false,               // [Boolean]
+        "FacelessMask": false,              // [Boolean]
+        "FakeMustache": false,              // [Boolean]
+        "FakeWhiteBeard": false,            // [Boolean]
+        "Reserved for future use": "The following items are not currently available in SPT 3.0.0",
+        "BaddiesRedBeard": false,           // [Boolean]
+        "BigPipe": false,                   // [Boolean]
+        "HockeyPlayerCaptain": false,       // [Boolean]
+        "HockeyPlayerBrawler": false,       // [Boolean]
+        "HockeyPlayerQuiet": false,         // [Boolean]
+        "DeathKnightMask": false,           // [Boolean]
+        "GloriousEMask": false              // [Boolean]
 }
 ```
--- mods/BandanaOfProtection/src/BoP.ts
+++ mods/BandanaOfProtection/src/BoP.ts
@@ -1,5 +1,5 @@
 /*
-    Mod Version | 3.0.0
+    Mod Version | 3.0.1
     Aki Version | >=3.0.0
     Copyright   | 2022 jbs4bmx
 */
@@ -40,7 +40,7 @@ class Bandana implements IMod
 
     public delayedLoad(container: DependencyContainer)
     {
-        const { MainArmor, HeadAreas, Resources } = require('./config.json');
+        const { MainArmor, HeadAreas, Resources, FaceCover } = require('./config.json');
         const logger = container.resolve<ILogger>("WinstonLogger");
         const db = container.resolve("DatabaseServer").getTables();
         const items = db.templates.items;
@@ -55,7 +55,9 @@ class Bandana implements IMod
 
         let armor = [];
         let segments = [];
+        let fab = "";
 
+        // Armor options
         if (typeof MainArmor.Head === "boolean") { if (MainArmor.Head === true) { armor.push("Head") } } else { armor.push("Head") }
         if (typeof MainArmor.Thorax === "boolean") { if (MainArmor.Thorax === true) { armor.push("Chest") } } else { armor.push("Chest") }
         if (typeof MainArmor.Stomach === "boolean") { if (MainArmor.Stomach === true) { armor.push("Stomach") } } else { armor.push("Stomach") }
@@ -68,10 +70,51 @@ class Bandana implements IMod
         if (typeof HeadAreas.Ears === "boolean") { if (HeadAreas.Ears === true) { segments.push("Ears") } } else { segments.push("Ears") }
         if (typeof HeadAreas.Eyes === "boolean") { if (HeadAreas.Eyes === true) { segments.push("Eyes") } } else { segments.push("Eyes") }
         if (typeof HeadAreas.Jaws === "boolean") { if (HeadAreas.Jaws === true) { segments.push("Jaws") } } else { segments.push("Jaws") }
+        // Cost and Durability options
         if (typeof Resources.RepairCost === "number") { if ((Resources.RepairCost < 1) || (Resources.RepairCost > 9999999)) { Resources.RepairCost = 1000 } } else { Resources.RepairCost = 1000 }
         if (typeof Resources.Durability === "number") { if ((Resources.Durability < 1) || (Resources.Durability > 9999999)) { Resources.Durability = 1500 } } else { Resources.Durability = 1500 }
         if (typeof Resources.traderPrice === "number") { if ((Resources.traderPrice < 1) || (Resources.traderPrice > 9999999)) { Resources.traderPrice = 69420 } } else { Resources.traderPrice = 69420 }
+        // Different face cover options
+        if (typeof FaceCover.HalfMask === "boolean") { if (FaceCover.HalfMask) { fab = "" } }
+        if (typeof FaceCover.GP5GasMask === "boolean") { if (FaceCover.GP5GasMask) { fab = "assets/content/items/equipment/facecover_gasmask_gp5/item_equipment_facecover_gasmask_gp5.bundle" } }
+        if (typeof FaceCover.GP7GasMask === "boolean") { if (FaceCover.GP7GasMask) { fab = "assets/content/items/equipment/facecover_gasmask_gp7/item_equipment_facecover_gasmask_gp7.bundle" } }
+        if (typeof FaceCover.Respirator === "boolean") { if (FaceCover.Respirator) { fab = "assets/content/items/equipment/facecover_gasmask_3m/item_equipment_facecover_gasmask_3m.bundle" } }
+        if (typeof FaceCover.DevBalaclava === "boolean") { if (FaceCover.DevBalaclava) { fab = "assets/content/items/equipment/facecover_balaclava/item_equipment_facecover_balaclava_development.bundle" } }
+        if (typeof FaceCover.JasonMask === "boolean") { if (FaceCover.JasonMask) { fab = "assets/content/items/equipment/facecover_halloween_jason/item_equipment_facecover_halloween_jason.bundle" } }
+        if (typeof FaceCover.MichealMask === "boolean") { if (FaceCover.MichealMask) { fab = "assets/content/items/equipment/facecover_halloween_michael/item_equipment_facecover_halloween_micheal.bundle" } }
+        if (typeof FaceCover.PestilyMask === "boolean") { if (FaceCover.PestilyMask) { fab = "assets/content/items/equipment/facecover_pestily/item_equipment_facecover_pestily.bundle" } }
+        if (typeof FaceCover.SmokeBalaclava === "boolean") { if (FaceCover.SmokeBalaclava) { fab = "assets/content/items/equipment/facecover_smoke/item_equipment_head_smoke.bundle" } }
+        if (typeof FaceCover.TagillaGorilla === "boolean") { if (FaceCover.TagillaGorilla) { fab = "assets/content/items/equipment/facecover_welding/item_equipment_facecover_welding_gorilla.bundle" } }
+        if (typeof FaceCover.TagillaUBEY === "boolean") { if (FaceCover.TagillaUBEY) { fab = "assets/content/items/equipment/facecover_welding/item_equipment_facecover_welding_kill.bundle" } }
+        if (typeof FaceCover.GhostBalaclava === "boolean") { if (FaceCover.GhostBalaclava) { fab = "assets/content/items/equipment/facecover_balaclavaskull/item_equipment_facecover_balaclavaskull.bundle" } }
+        if (typeof FaceCover.MomexBalaclava === "boolean") { if (FaceCover.MomexBalaclava) { fab = "assets/content/items/equipment/facecover_nomexbalaclava/item_equipment_facecover_nomexbalaclava.bundle" } }
+        if (typeof FaceCover.ColdFearBalaclava === "boolean") { if (FaceCover.ColdFearBalaclava) { fab = "assets/content/items/equipment/facecover_coldgear/item_equipment_facecover_coldgear.bundle" } }
+        if (typeof FaceCover.Rivals2021Balaclava === "boolean") { if (FaceCover.Rivals2021Balaclava) { fab = "assets/content/items/equipment/facecover_coldgear/item_equipment_facecover_coldgear_twitch.bundle" } }
+        if (typeof FaceCover.Balaclava === "boolean") { if (FaceCover.Balaclava) { fab = "assets/content/items/equipment/facecover_balaclava/item_equipment_facecover_balaclava.bundle" } }
+        if (typeof FaceCover.RoninBallistic === "boolean") { if (FaceCover.RoninBallistic) { fab = "assets/content/items/equipment/facecover_devtac/item_equipment_facecover_devtac.bundle" } }
+        if (typeof FaceCover.TwitchRivals2020Mask === "boolean") { if (FaceCover.TwitchRivals2020Mask) { fab = "assets/content/items/equipment/facecover_redflame/item_equipment_facecover_redflame_twitch.bundle" } }
+        if (typeof FaceCover.TwitchRivals2020HalfMask === "boolean") { if (FaceCover.TwitchRivals2020HalfMask) { fab = "assets/content/items/equipment/facecover_shroud/item_equipment_facecover_shroud_twitch.bundle" } }
+        if (typeof FaceCover.GreenShemagh === "boolean") { if (FaceCover.GreenShemagh) { fab = "assets/content/items/equipment/facecover_shemagh/item_equipment_facecover_shemagh.bundle" } }
+        if (typeof FaceCover.TanShemagh === "boolean") { if (FaceCover.TanShemagh) { fab = "assets/content/items/equipment/facecover_shemagh_02/item_equipment_facecover_shemagh_02.bundle" } }
+        if (typeof FaceCover.ShroudMask === "boolean") { if (FaceCover.ShroudMask) { fab = "assets/content/items/equipment/facecover_shroud/item_equipment_facecover_shroud.bundle" } }
+        if (typeof FaceCover.ShatteredMask === "boolean") { if (FaceCover.ShatteredMask) { fab = "assets/content/items/equipment/facecover_shatteredmask/item_equipment_facecover_shatteredmask.bundle" } }
+        if (typeof FaceCover.DeadlySkull === "boolean") { if (FaceCover.DeadlySkull) { fab = "assets/content/items/equipment/facecover_skullmask/item_equipment_facecover_skullmask.bundle" } }
+        if (typeof FaceCover.NeopreneMask === "boolean") { if (FaceCover.NeopreneMask) { fab = "assets/content/items/equipment/facecover_redflame/item_equipment_facecover_redflame.bundle" } }
+        if (typeof FaceCover.GhoulMask === "boolean") { if (FaceCover.GhoulMask) { fab = "assets/content/items/equipment/facecover_halloween_vampire/item_equipment_facecover_halloween_vampire.bundle" } }
+        if (typeof FaceCover.SlenderMask === "boolean") { if (FaceCover.SlenderMask) { fab = "assets/content/items/equipment/facecover_halloween_slander/item_equipment_facecover_halloween_slander.bundle" } }
+        if (typeof FaceCover.FacelessMask === "boolean") { if (FaceCover.FacelessMask) { fab = "assets/content/items/equipment/facecover_halloween_kaonasi/item_equipment_facecover_halloween_kaonasi.bundle" } }
+        if (typeof FaceCover.FakeMustache === "boolean") { if (FaceCover.FakeMustache) { fab = "assets/content/items/equipment/mustache/item_equipment_mustache.bundle" } }
+        if (typeof FaceCover.FakeWhiteBeard === "boolean") { if (FaceCover.FakeWhiteBeard) { fab = "assets/content/items/equipment/item_beard/item_beard.bundle" } }
+        // Reserved for future use when SPT gets updated to EFT 12.12.30.xxxxx
+        //if (typeof FaceCover.BaddiesRedBeard === "boolean") { if (FaceCover.BaddiesRedBeard) { fab = "assets/content/items/equipment/item_equipment_facecover_beard_red.bundle" } }
+        //if (typeof FaceCover.BigPipe === "boolean") { if (FaceCover.BigPipe) { fab = "assets/content/items/equipment/item_equipment_facecover_pipe.bundle" } }
+        //if (typeof FaceCover.HockeyPlayerCaptain === "boolean") { if (FaceCover.HockeyPlayerCaptain) { fab = "assets/content/items/equipment/item_equipment_facecover_hockey_01.bundle" } }
+        //if (typeof FaceCover.HockeyPlayerBrawler === "boolean") { if (FaceCover.HockeyPlayerBrawler) { fab = "assets/content/items/equipment/item_equipment_facecover_hockey_02.bundle" } }
+        //if (typeof FaceCover.HockeyPlayerQuiet === "boolean") { if (FaceCover.HockeyPlayerQuiet) { fab = "assets/content/items/equipment/item_equipment_facecover_hockey_03.bundle" } }
+        //if (typeof FaceCover.DeathKnightMask === "boolean") { if (FaceCover.DeathKnightMask) { fab = "assets/content/items/equipment/item_equipment_facecover_boss_blackknight.bundle" } }
+        //if (typeof FaceCover.GloriousEMask === "boolean") { if (FaceCover.GloriousEMask) { fab = "assets/content/items/equipment/item_equipment_facecover_glorious.bundle" } }
 
+        if (!( fab === "" )) { items["BandanaOfProtection00xxx"]._props.Prefab.path = fab; }
         items["BandanaOfProtection00xxx"]._props.CreditsPrice = Resources.traderPrice;
         items["BandanaOfProtection00xxx"]._props.RepairCost = Resources.RepairCost;
         items["BandanaOfProtection00xxx"]._props.Durability = Resources.Durability;
