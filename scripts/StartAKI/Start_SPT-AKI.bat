64b0565e0792b558939ca96b88c530302a579c72
Jace <livingfern1@live.com>
2024-02-11 16:16:57 -0500

    Update Start_SPT-AKI.bat
    Add more automation

--- scripts/StartAKI/Start_SPT-AKI.bat
+++ scripts/StartAKI/Start_SPT-AKI.bat
@@ -1,5 +1,6 @@
 @echo off
 title "SPT-AKI Start Up Script"
+REM SPT-AKI START UP SCRIPT
 :: SYNOPSIS
 ::    Launches a game! :)
 :: DESCRIPTION
@@ -7,110 +8,150 @@ title "SPT-AKI Start Up Script"
 :: OUTPUTS
 ::    {ScriptDirectory}\Start_SPT-AKI.log
 :: NOTES
-::    File Name:      | Start_SPT-AKI.bat
 ::    Author:         | jbs4bmx
 ::    Date:           | [ddMMyyyy] 18.10.2020
-::    Edit:           | [ddMMyyyy] 24.07.2022
-::    Script Version: | 0.6
+::    Edit:           | [ddMMyyyy] 11.02.2024
+::    Script Version: | 0.7
 ::    Help Requests:  | https://discordapp.com/users/510535592833056777
-::    License:        | https://opensource.org/licenses/MIT | Copyright (c) 2022 jbs4bmx
-
-::    Special Note:   | Processor Affinity (Hyper-Threaded CPUs) (SET ON LINE 108)
-::                    | 2-CORE  = 5  (CPUs 0,2)
-::                    |           10 (CPUs 1,3)
-::                    | 4-CORE  = 85  (CPUs 0,2,4,6)
-::                    |           170 (CPUs 1,3,5,7)
-::                    | 6-CORE  = 1365 (CPUs 0,2,4,6,8,10)
-::                    |           2730 (CPUs 1,3,5,7,9,11)
-::                    | 8-CORE  = 21845 (CPUs 0,2,4,6,8,10,12,14)
-::                    |           43690 (CPUs 1,3,5,7,9,11,13,15)
-::                    | 10-CORE = 349525 (CPUs 0,2,4,6,8,10,12,14,16,18)
-::                    |           699050 (CPUs 1,3,5,7,9,11,13,15,17,19)
-::                    | 12-CORE = 5592405  (CPUs 0,2,4,6,8,10,12,14,16,18,20,22)
-::                    |           11184810 (CPUs 1,3,5,7,9,11,13,15,17,19,21,23)
-::                    | 14-CORE = 89478485  (CPUs 0,2,4,6,8,10,12,14,16,18,20,22,24,26)
-::                    |           178956970 (CPUs 1,3,5,7,9,11,13,15,17,19,21,23,25,27)
-::                    | 16-CORE = 1431655765 (CPUs 0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30)
-::                    |           2863311530 (CPUs 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31)
+::    License:        | https://opensource.org/licenses/MIT | Copyright (c) 2024 jbs4bmx
+::                    | ----------------------------------------------------------------------------------------------------------
+::    Special Note:   | Processor Affinity for Hyper-Threaded CPUs (SET ON LINE 108)
+::                    | ----------------------------------------------------------------------------------------------------------
+::                    | CORES |  Affinity  |             CPU Cores                      | Core Type
+::                    |-------|------------|--------------------------------------------|-----------------------------------------
+::                    |     2 |          5 | 0,2                                        | Physical
+::                    |       |         10 | 1,3                                        | Virtual
+::                    |-------|------------|--------------------------------------------|-----------------------------------------
+::                    |     4 |         85 | 0,2,4,6                                    | Physical
+::                    |       |        170 | 1,3,5,7                                    | Virtual
+::                    |-------|------------|--------------------------------------------|-----------------------------------------
+::                    |     6 |       1365 | 0,2,4,6,8,10                               | Physical
+::                    |       |       2730 | 1,3,5,7,9,11                               | Virtual
+::                    |-------|------------|--------------------------------------------|-----------------------------------------
+::                    |     8 |      21845 | 0,2,4,6,8,10,12,14                         | Physical
+::                    |       |      43690 | 1,3,5,7,9,11,13,15                         | Virtual
+::                    |-------|------------|--------------------------------------------|-----------------------------------------
+::                    |    10 |     349525 | 0,2,4,6,8,10,12,14,16,18                   | Physical
+::                    |       |     699050 | 1,3,5,7,9,11,13,15,17,19                   | Virtual
+::                    |-------|------------|--------------------------------------------|-----------------------------------------
+::                    |    12 |    5592405 | 0,2,4,6,8,10,12,14,16,18,20,22             | Physical
+::                    |       |   11184810 | 1,3,5,7,9,11,13,15,17,19,21,23             | Virtual
+::                    |-------|------------|--------------------------------------------|-----------------------------------------
+::                    |    14 |   89478485 | 0,2,4,6,8,10,12,14,16,18,20,22,24,26       | Physical
+::                    |       |  178956970 | 1,3,5,7,9,11,13,15,17,19,21,23,25,27       | Virtual
+::                    |-------|------------|--------------------------------------------|-----------------------------------------
+::                    |    16 | 1431655765 | 0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30 | Physical
+::                    |       | 2863311530 | 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31 | Virtual
+::                    | ----------------------------------------------------------------------------------------------------------
+::                    | The process can be set using this code (example for 8-core CPU)...
+::                    | PowerShell "$Process = Get-Process EscapeFromTarkov; $Process.ProcessorAffinity=21845; $Process.PriorityClass=[System.Diagnostics.ProcessPriorityClass]::AboveNormal;"
+::                    | ----------------------------------------------------------------------------------------------------------
 ::                    | The process can also be automated by using the following line of code...
 ::                    | PowerShell "$proc = Get-WmiObject -class Win32_processor; $affinity=$proc.numberoflogicalprocessors - $proc.numberofcores; switch ($affinity){2 { $affinity = 5}4{$affinity = 85}6{$affinity = 1365}8{$affinity = 21845}10{$affinity = 349525}12{$affinity = 5592405}14{$affinity = 89478485}16{$affinity = 1431655765}}; if ($affinity){$Process = Get-Process EscapeFromTarkov; $Process.ProcessorAffinity=$affinity; $Process.PriorityClass=[System.Diagnostics.ProcessPriorityClass]::AboveNormal;}"
-
-:: ---------------------------------------------------------[Initializations]--------------------------------------------------------
-set ver="0.3"
-setlocal enabledelayedexpansion
+::                    | ----------------------------------------------------------------------------------------------------------
+::                    | Note: Affinity feature is currently not in place for Intel's PE core CPUs. ETA:Unknown
 
 :: ----------------------------------------------------------[Declarations]----------------------------------------------------------
+setlocal enabledelayedexpansion
+set ver="0.7"
 set server=%~dp0Aki.Server.exe
 set client=%~dp0Aki.Launcher.exe
+set config=%~dp0%Aki_Data\Server\database\server.json
+set string=
 set log=%~dp0Start_SPT-AKI.log
+set fileChkCt=0
+set launchErrCt=0
+
+:: ---------------------------------------------------------[Initializations]--------------------------------------------------------
+:: Parsing the server config file so we can get and use the port value later
+for /f "delims=" %%x in (%config%) do set "string=!string!%%x"
+set string=%string:"=%
+set "string=%string:~2,-2%"
+set "string=%string:: ==%"
+set "%string:, =" & set "%"
 
 :: -----------------------------------------------------------[Execution]------------------------------------------------------------
+:fileVerificationChecks
 ::verify files exist
-echo.Verifying game files...
+echo.Verifying game files... && echo.Verifying game files... >>%log%
 if not exist %server% (
-    echo.^[ERROR^] Server.exe is missing or incorrectly placed.
-    echo.^[ERROR^] Server.exe is missing or incorrectly placed. >> %log%
+    set /a fileChkCt=fileChkCt+1
+    echo.^[ERROR^] %server% not found. >>%log%
 ) else (
-    echo.^[SUCCESS^] Server.exe found.
-    echo.^[SUCCESS^] Server.exe found. >> %log%
+    echo.%server% is present. >>%log%
 )
 if not exist %client% (
-    echo.^[ERROR^] Launcher.exe is missing or incorrectly placed.
-    echo.^[ERROR^] Launcher.exe is missing or incorrectly placed. >> %log%
+    set /a fileChkCt=fileChkCt+1
+    echo.^[ERROR^] %client% not found. >>%log%
 ) else (
-    echo.^[SUCCESS^] Launcher.exe found.
-    echo.^[SUCCESS^] Launcher.exe found. >> %log%
+    echo.%client% is present. >>%log%
+)
+if %fileChkCt% gtr 0 (
+    echo.^[ERROR^] One or more files are missing or incorrectly placed. && echo.Check the log file for details.
+    pause
+    goto exitScript
 )
 ping -n 3 127.0.0.1 >NUL 2>&1 || ping -n %1 ::1 >NUL 2>&1
-echo.
-echo.
 
+
+:startServer
 :: Launch Game Server
-echo Starting Server...
-::start "Server" /Min /i %server%
+echo. && echo. && echo.Starting %server%...
 start "Server" /i %server%
 if errorlevel 0 (
-    echo.^[SUCCESS^] Server started successfully.
-    echo.^[SUCCESS^] Server started successfully. >> %log%
-    ping -n 10 127.0.0.1 >NUL 2>&1 || ping -n %1 ::1 >NUL 2>&1
-    :: Launch Game Client
-    echo.Starting Launcher...
-    start "Launcher" %client%
-    if errorlevel 0 (
-        echo.^[SUCCESS^] Launcher started successfully.
-        echo.^[SUCCESS^] Launcher started successfully. >> %log%
-        ping -n 3 127.0.0.1 >NUL 2>&1 || ping -n %1 ::1 >NUL 2>&1
-    ) else (
-        echo.^[ERROR^] Failed to start Launcher successfully.
-        echo.^[ERROR^] Failed to start Launcher successfully. >> %log%
-        pause
-    )
+    ping -n 2 127.0.0.1 >NUL 2>&1 || ping -n %1 ::1 >NUL 2>&1
+    GOTO serverStatusCheck
 ) else (
-    echo.^[ERROR^] Failed to start Server successfully.
-    echo.^[ERROR^] Failed to start Server successfully. >> %log%
+    echo.    ^| ^[ERROR^] Failed to start %server% successfully. && echo.^[ERROR^] Failed to start %server% successfully. >>%log%
     pause
+    goto exitScript
 )
-echo.
-echo.
 
+
+:serverStatusCheck
+:: Cycle until we know that the server is running
+echo.    ^| %server% started - awaiting status...
+netstat -o -n -a | findstr %Port% >NUL 2>&1 && if errorlevel 0 goto startClient
+ping -n 2 127.0.0.1 >NUL 2>&1 || ping -n %1 ::1 >NUL 2>&1
+goto serverStatusCheck
+
+
+:startClient
+:: Launch Game Client
+echo.    ^| ^[SUCCESS^] %server% started successfully.
+echo.^[SUCCESS^] %server% started successfully. >>%log%
+echo. && echo. && echo.Starting Launcher...
+start "Launcher" %client%
+if errorlevel 0 (
+    echo.    ^| ^[SUCCESS^] %client% started successfully. && echo.^[SUCCESS^] %client% started successfully. >>%log%
+    ping -n 3 127.0.0.1 >NUL 2>&1 || ping -n %1 ::1 >NUL 2>&1
+    goto setAffinity
+) else (
+    echo.    ^| ^[ERROR^] Failed to start %client% successfully. && echo.^[ERROR^] Failed to start %client% successfully. >>%log%
+    pause
+    goto exitScript
+)
+
+
+:setAffinity
 :: Wait for EFT to start and then set Processor affinity
-echo.Waiting for EscapeFromTarkov process...
-:LOOP
+echo. && echo. && echo.Waiting for EscapeFromTarkov process...
+:loopThatShit
 tasklist | find /I "EscapeFromTarkov.exe" >NUL 2>&1
 if errorlevel 1 (
-    ping -n 5 127.0.0.1 >NUL 2>&1 || ping -n %1 ::1 >NUL 2>&1
-    goto LOOP
+    ping -n 3 127.0.0.1 >NUL 2>&1 || ping -n %1 ::1 >NUL 2>&1
+    goto loopThatShit
 )
-echo.
-:: Allow the game a short time to load
+:: PING null for 30 seconds to allow the game client enough time to load
 ping -n 30 127.0.0.1 >NUL 2>&1 || ping -n %1 ::1 >NUL 2>&1
-echo.Setting EFT processor affinity...
-PowerShell "$Process = Get-Process EscapeFromTarkov; $Process.ProcessorAffinity=21845; $Process.PriorityClass=[System.Diagnostics.ProcessPriorityClass]::AboveNormal;"
+echo.    ^| EFT is running - setting EFT processor affinity...
+PowerShell "$proc = Get-WmiObject -class Win32_processor; $affinity=$proc.numberoflogicalprocessors - $proc.numberofcores; switch ($affinity){2 { $affinity = 5}4{$affinity = 85}6{$affinity = 1365}8{$affinity = 21845}10{$affinity = 349525}12{$affinity = 5592405}14{$affinity = 89478485}16{$affinity = 1431655765}}; if ($affinity){$Process = Get-Process EscapeFromTarkov; $Process.ProcessorAffinity=$affinity; $Process.PriorityClass=[System.Diagnostics.ProcessPriorityClass]::AboveNormal;}"
 ping -n 3 127.0.0.1 >NUL 2>&1 || ping -n %1 ::1 >NUL 2>&1
 
-echo.
-echo.Exiting script...
+
+:exitScript
+echo. && echo. && echo.Exiting script...
 ping -n 3 127.0.0.1 >NUL 2>&1 || ping -n %1 ::1 >NUL 2>&1
-echo.>> %log%
+echo.>>%log%
 popd
 exit 0
